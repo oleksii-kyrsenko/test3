c617c9fa60788e490320647a56e85c94b444e459
kirsenkodanit <a.kirs@ukr.net>
2020-06-11 11:12:36 +0300

    Updates

---
+++ precache-manifest.81d77c9b12bce7552cc5908be6125ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb60108aeb7f68f4e6e7b1e1de5b9fed",
+    "url": "/test3/index.html"
+  },
+  {
+    "revision": "3413d4f22e92eafa4c55",
+    "url": "/test3/static/css/main.3c8a1772.chunk.css"
+  },
+  {
+    "revision": "6de4bc44a4860c0c5142",
+    "url": "/test3/static/js/2.938200dc.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/test3/static/js/2.938200dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3413d4f22e92eafa4c55",
+    "url": "/test3/static/js/main.7b12758f.chunk.js"
+  },
+  {
+    "revision": "eebda7b009fbd0c0f4b3",
+    "url": "/test3/static/js/runtime-main.e5be65d6.js"
+  }
+]);
